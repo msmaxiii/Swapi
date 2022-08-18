7be18f15b72a6abe14fbbfc884978af15d2dab3f
msmaxiii <maxwellrg@gmail.com>
2022-08-17 23:46:58 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('connected');
+
+const resBtn = document.querySelector("residents-button");
+
+function requestSubmit(e){
+    e.preventDefault()
+    
+   console.log()
+}
+// resBtn.addEventListener("click",submitRequest);
